38aafd63f35bb6dde02f31831073de6924c0600d
MMSHRIKARA <shrikara21@gmail.com>
2022-09-06 12:30:50 +0530

    Second commit

---
+++ jwt/src/main/java/com/kanbanboard/KanbanBoardApplication.java
@@ -0,0 +1,28 @@
+package com.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+//@ComponentScan("com.kanbanboard.controller" +"com.kanbanboard.service")
+public class KanbanBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanBoardApplication.class, args);
+    }
+    
+    @Bean
+	public Docket productApi() {
+	return new Docket(DocumentationType.SWAGGER_2).select()
+	.apis(RequestHandlerSelectors.basePackage("com.kanbanboard")).build();
+	}
+
+}
